99991f917aa811e5b59518c6d8d54c59d49d7097
Tkacik <jan.tkacik@merck.com>
2017-12-01 21:17:02 +0100

    First message

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 
 app = Flask(__name__)
 
 
-@app.route('/hackaton/backend', methods=['GET'])
+@app.route('/msb/getscore', methods=['POST'])
 def predictive_maintanance_zeroloss():
-    return jsonify({'response': 1})
+    try:
+        indata = json.loads(request.data.decode("utf-8"))
+        return jsonify(
+            {
+                "results":
+                [
+                    {
+                        "Score": 43242,
+                        "Product": {
+                            "Name": "Clovek",
+                            "Price": 321312
+                        },
+                        "ProCons":
+                        [
+                            {
+                                "Type": "Pro",
+                                "Reason": "Vacsi penis o 3cm"
+                            },
+                            {
+                                "Type": "Cons",
+                                "Reason": "Horsi ksicht"
+                            }
+                        ]
+                    }
+                ]
+            }
+        )
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 
 if __name__ == '__main__':

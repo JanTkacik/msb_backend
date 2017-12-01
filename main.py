8286b900ca6e41b0ada5f82135531e9237e0dd1b
Tkacik <jan.tkacik@merck.com>
2017-12-01 23:39:33 +0100

    Random scoring

--- main.py
+++ main.py
@@ -1,41 +1,62 @@
 #!flask/bin/python
 from flask import Flask, jsonify, request
 import json
+import os
+import traceback
+from scoring.scorer import Scorer
+from product.productrepo import FileProductRepository
+from product.userfilter import UserFilter
 
+PROD_ID_TAG = "ProdId"
+PRICE_RANGE_TAG = "PriceRange"
+PRICE_RANGE_FROM_TAG = "from"
+PRICE_RANGE_TO_TAG = "to"
+IS_BASE_TAG = "IsBase"
+SCORE_TAG = "Score"
+PRODUCT_TAG = "Product"
+
+scorer = Scorer()
+repo = FileProductRepository(os.environ['DATA_PATH'])
 app = Flask(__name__)
 
 
 @app.route('/msb/getscore', methods=['POST'])
-def predictive_maintanance_zeroloss():
+def get_score():
     try:
         indata = json.loads(request.data.decode("utf-8"))
-        return jsonify(
-            {
-                "results":
-                [
+        if PROD_ID_TAG in indata:
+            productId = indata[PROD_ID_TAG]
+            pricefrom = None
+            priceto = None
+            if PRICE_RANGE_TAG in indata:
+                pricerange = indata[PRICE_RANGE_TAG]
+                if PRICE_RANGE_FROM_TAG in pricerange:
+                    pricefrom = float(pricerange[PRICE_RANGE_FROM_TAG])
+                if PRICE_RANGE_TO_TAG in pricerange:
+                    priceto = float(pricerange[PRICE_RANGE_TO_TAG])
+            baseproduct = repo.getProductByItemId(productId)
+            if baseproduct is None:
+                return jsonify({'error': "Product not found"})
+            userfilter = UserFilter(
+                baseproduct.getCategoryTree(), 10, pricefrom, priceto)
+            otherproducts = repo.getProductsByUserFilter(userfilter)
+            out = {}
+            results = []
+            scores = [scorer.getScore(p) for p in otherproducts]
+            isbase = [p.getItemId() == productId for p in otherproducts]
+            for i in range(len(otherproducts)):
+                results.append(
                     {
-                        "Score": 43242,
-                        "Product": {
-                            "Name": "Clovek",
-                            "Price": 321312
-                        },
-                        "ProCons":
-                        [
-                            {
-                                "Type": "Pro",
-                                "Reason": "Vacsi penis o 3cm"
-                            },
-                            {
-                                "Type": "Cons",
-                                "Reason": "Horsi ksicht"
-                            }
-                        ]
-                    }
-                ]
-            }
-        )
+                        IS_BASE_TAG: isbase[i],
+                        SCORE_TAG: scores[i],
+                        PRODUCT_TAG: otherproducts[i].raw
+                    })
+            out["results"] = results
+            return jsonify(out)
+        else:
+            return jsonify({'error': "Product id not specified"})
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return jsonify({'error': str(e), 'trace': traceback.format_exc()})
 
 
 if __name__ == '__main__':
---
+++ scoring/scorer.py
@@ -0,0 +1,9 @@
+import random
+
+
+class Scorer:
+    def __init__(self):
+        pass
+
+    def getScore(self, product):
+        return random.uniform(0, 100)
